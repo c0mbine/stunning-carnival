abcb14254f04eb9905f827f5d1e93f705c0cef7d
Steven Jakus <flying.code.tv@gmail.com>
2019-10-08 10:37:53 -0500

    Added Goodbye restful listener

---
+++ src/main/java/hello/Goodbye.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Goodbye {
+
+    private final long id;
+    private final String content;
+
+    public Goodbye(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getID(){
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,14 +4,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//The RestController makes the class a controller where every method returns a domain object instead of a view.
 @RestController
 public class GreetingController{
 
-    private static final String template = "Hello, %s!";
+    private static final String greetingTempalate = "Howdy, %s!";
+    private static final String goodbyeTemplate = "Goodbye, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name){
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(greetingTempalate, name));
+    }
+
+    @RequestMapping("/goodbye")
+    public Goodbye goodbye(@RequestParam(value="name", defaultValue = "World") String name){
+        return new Goodbye(counter.incrementAndGet(), String.format(goodbyeTemplate, name));
     }
 }
