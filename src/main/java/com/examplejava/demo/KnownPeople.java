451f2f0f0e8aace48a4fc432b8ff92960512dbee
Steven Jakus <flying.code.tv@gmail.com>
2019-10-10 13:46:14 -0500

    Added basic comments for ES DB connections

--- src/main/java/com/examplejava/demo/KnownPeople.java
+++ src/main/java/com/examplejava/demo/KnownPeople.java
@@ -10,24 +10,28 @@ public class KnownPeople {
     private static Set<Person> peopleSet = new LinkedHashSet<>();
 
     public static void addPerson(String name) {
+        //Insert into DB, only unique entries
         peopleSet.add(new Person(name));
     }
 
     public static Boolean doesPersonExist(Person person) {
+        //Select Person 
         return (peopleSet.contains(person) ? true : false);
     }
 
     public static void removePerson(String name) {
-        // find person by name
+        // DELETE entry from DB by string
     }
 
     public static void removePerson(Person person) {
+        //Most likely will not be used in DB version
         if (doesPersonExist(person)) {
             peopleSet.remove(person);
         }
     }
 
     public static int size() {
+        //query DB for KnownPeople Table size
         return peopleSet.size();
     }
 
@@ -56,6 +60,7 @@ public class KnownPeople {
 
     @Override
     public String toString() {
+        // This will report out a minified JSON of all the people in th DB
         return peopleSet.toString();
     }
 
